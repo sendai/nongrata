20650e41e8686c76f5673b7ad60914155732e656
Paul Ewing <paul.ewing.ctr@hpcmo.hpc.mil>
2012-05-02 20:34:32 -0500

    Use lein-cljsbuild.

--- project.clj
+++ project.clj
@@ -5,5 +5,13 @@
                  [noir "1.3.0-beta3"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]]
   :jvm-opts ["-server -Xmx512m"] 
-  :main nongrata.server)
-
+  :plugins [[lein-cljsbuild "0.1.8"]]
+  :main nongrata.server
+  :cljsbuild
+  {:builds
+  [{:source-path "src-cljs",
+                 :compiler
+                 {:output-to "resources/public/js/cljs.js",
+                 :externs ["externs/jquery.js"],
+                 :optimizations :simple,
+                 :pretty-print true}}]})
